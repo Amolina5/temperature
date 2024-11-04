da883a65f3922192b4a249e525872779baa19ab2
Amolina5 <amolina555@hotmail.com>
2024-11-04 17:46:08 -0600

    variables

---
+++ scripts/scripts.js
@@ -0,0 +1,48 @@
+console.log("scripts");
+
+let myName="Alex"; // declaration of variable and assigning vaue
+var myAge = 26;
+let isStudent = true;
+let myLastName= "Molina";
+let myHeight= 72;
+
+// display the values on the console you can use commas to display everthing in the same line
+console.log(myName);
+console.log(myAge);
+console.log(myLastName);
+console.log(myHeight);
+
+// concatenation
+
+console.log("My name is " + myName, "I am " + myAge, "years old and my height is " + myHeight);
+
+// operations
+
+let num1 = 10;
+let num2=2;
+
+addition = num1+num2;
+subtraction = num1 - num2;
+multication = num1 * num2;
+
+console.log( " Addtion is: " + addition);
+console.log(" subtraction is: " + subtraction);
+console.log(" multication is: " + multication );
+
+let a;
+const pi = 3.1416;
+let r = 3;
+
+a= pi * (r*r);
+console.log(" the area of a circule with a radius of " + r + " is " + a);
+
+let aVar = 20;
+let otherVar =30;
+
+console.log(aVar,otherVar);// 50 60
+
+const weekDays = 5;
+
+//weekDays=10; // the constant cant change
+
+console.log(weekDays)
