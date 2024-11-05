45cc78025526db77f9c17f6fa4b96d42855c2d31
Amolina5 <amolina555@hotmail.com>
2024-11-04 18:45:33 -0600

    variables

---
+++ scripts/funnystory.js
@@ -0,0 +1,31 @@
+console.log("scripts");
+
+// 1. Declare Variables
+// Using let
+let charater1 = "Thad"
+let charater2 = "Trash Monster";
+let action = "Trowing a fit";
+let object = "eating the cookies";
+let place = "alley";
+let time = "a gloomie night";
+
+// Using "const"
+const emotion = "triumph of rejoice"
+
+// creat the store
+console.log("-- funny story----");
+console.log( charater1+ " was walking through the city of New York before he heard a lot of munching coming form the " + place+ "." + charater1 + " began to investgate the noise as he was searching the " + place + " he stumbled apond a bag of cookies so he took it thinking it would be a amazing snack for later. as he began to leave he heard banging and crying coming from a trash can then the lid popped open and it was the " + charater2 + " and he was " + action + charater1 + " paniced thinking what should he do then he remmbeed the cookies so he threw them at " + charater2 + "he began to" + object + " then let a loud " + emotion + charater1 + " will never forget that" + time);
+
+// diffrent vaules
+console.log ("---Diffrent story---");
+
+// change values to make new story
+let character3 = "The Rock";
+let character4 = "Chris Rock";
+let action2 = "tap dancing";
+let object2 = "sacred rock";
+let place2 = "hot airballoon";
+let time2 = "summer";
+
+console.log("---Different Story---");
+console.log(character3 + " was flying high in the " + place2 + " admiring the beautiful " + time2 + " view when he noticed a glimmering " + object2 + " on the edge of the basket. With no thought, " + character3 + " reached out and grabbed it, only to hear a faint whisper in the air. Suddenly," + character4 + " pops up from the other side of the balloon basket and starts " + action2 + " wildly. " + character3 + " starts panicking, not knowing what to expect. " + character4 + " notices the " + object2 + " in " + character3 +"'s hand and exclaims that it is a " + emotion + " " + object2 + " that brings luck and fortune. Together they make elaborate schemes to make the best use of the " + object2 + " and " + character3 +"realizes how lucky he is to have met a great companion such as " + character4);
